f69b0a0197b1f2d0f41e65512b6f1f63b5a2782a
Sakib Rezoan Reza <bsse0611@iit.du.ac.bd>
2023-04-22 23:17:24 +0600

    Student-Result Management system refactored

---
+++ src/main/java/com/reza/srms/responses/CourseWiseResultResponse.java
@@ -0,0 +1,27 @@
+package com.reza.srms.responses;
+
+import com.reza.srms.entities.CourseWiseResult;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class CourseWiseResultResponse implements Serializable {
+    private Long id;
+    private Long courseId;
+    private Float obtainedMarks;
+    private Float gpa;
+    private Long studentId;
+
+    public static CourseWiseResultResponse makeResponse(CourseWiseResult courseWiseResult) {
+        CourseWiseResultResponse response = new CourseWiseResultResponse();
+        response.setId(courseWiseResult.getId());
+        response.setCourseId(courseWiseResult.getCourse().getId());
+        response.setObtainedMarks(courseWiseResult.getObtainedMarks());
+        response.setGpa(courseWiseResult.getGpa());
+        response.setStudentId(courseWiseResult.getStudent().getId());
+        return response;
+    }
+}
